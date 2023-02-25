1444bf45821c547ef7cae0c999dc8c0b9cb29181
Blake Mallory <blakecmallory@gmail.com>
2023-02-25 00:23:56 -0500

    updated deploy script to have more options

--- inference/deploy.py
+++ inference/deploy.py
@@ -1,18 +1,27 @@
 import mii
 import os
 from transformers import AutoModelForCausalLM
-# model_name = "EleutherAI/gpt-neo-2.7B"
-model_name = "EleutherAI/gpt-j-6B"
+import argparse
 
-# model_name = AutoModelForCausalLM.from_pretrained(model_name)
+parser = argparse.ArgumentParser()
+parser.add_argument("-m","--model_name", type=str, default="EleutherAI/gpt-j-6B")
+parser.add_argument("-tp","--tensor_parallel", type=int, default=1)
+
+# model_name = "EleutherAI/gpt-neo-2.7B"
+
+args = parser.parse_args()
+model_name = args.model_name
+tensor_parallel = args.tensor_parallel
 
 # dtype = "int8"
 dtype = "fp16"
 
 skip_model_check = True
+deployment_name = "deployment"
+max_tokens = 2048
 
-mii_configs = {"tensor_parallel": 1, "dtype": dtype,"skip_model_check": skip_model_check}
+mii_configs = {"tensor_parallel": tensor_parallel, "dtype": dtype,"skip_model_check": skip_model_check, "max_tokens": max_tokens}
 mii.deploy(task="text-generation",
            model=model_name,
-           deployment_name="deployment",
+           deployment_name=deployment_name,
            mii_config=mii_configs)
