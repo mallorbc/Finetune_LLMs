d3de300f7ccdaa26c5f5be258080d27eb27ce898
Blake Mallory <blakecmallory@gmail.com>
2023-03-14 21:02:54 -0400

    added files for llama and added file that analyzes number of tokens in a dataset

---
+++ quotes_dataset/analyze_dataset.py
@@ -0,0 +1,32 @@
+from transformers import AutoTokenizer
+import argparse
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m","--model_name", type=str, default="EleutherAI/gpt-j-6B")
+args = parser.parse_args()
+
+
+tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+
+train_df = pd.read_csv('./llama/train.csv')
+valid_df = pd.read_csv('./llama/validation.csv')
+combined_df = pd.concat([train_df, valid_df])
+
+#loop through each row under text column and tokenize using iterows
+all_sizes = []
+for index, row in tqdm(combined_df.iterrows(), total=len(combined_df)):
+    text = row['text']
+    tokenized_text = tokenizer(text, return_tensors="np")
+    tokenized_size = np.shape(tokenized_text['input_ids'])[-1]
+    all_sizes.append(tokenized_size)
+
+print("Max size: ", max(all_sizes))
+print("Min size: ", min(all_sizes))
+print("Mean size: ", np.mean(all_sizes))
+print("Median size: ", np.median(all_sizes))
+
+
+
