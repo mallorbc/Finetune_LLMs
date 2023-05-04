dc50e8015fa2549d64840e4f81e7d5e1b5e01e2b
Blake Mallory <blakecmallory@gmail.com>
2023-05-04 01:06:56 -0400

    fixed increased max shard size

--- llama/get_llama.py
+++ llama/get_llama.py
@@ -4,17 +4,17 @@ import os
 #13B model
 model_name = "TheBloke/vicuna-13B-1.1-HF"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name,max_shard_size="100GB")
+model = AutoModelForCausalLM.from_pretrained(model_name)
 output_path = "./local_vicuna13B"
 os.makedirs(output_path, exist_ok=True)
 tokenizer.save_pretrained(output_path)
-model.save_pretrained(output_path)
+model.save_pretrained(output_path,max_shard_size="100GB")
 
 #7B model
 model_name = "TheBloke/vicuna-7B-1.1-HF"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name,max_shard_size="100GB")
+model = AutoModelForCausalLM.from_pretrained(model_name)
 output_path = "./local_vicuna7B"
 os.makedirs(output_path, exist_ok=True)
 tokenizer.save_pretrained(output_path)
-model.save_pretrained(output_path)
+model.save_pretrained(output_path,max_shard_size="100GB")
