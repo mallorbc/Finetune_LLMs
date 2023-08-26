e79863f951d88cde13c6d0458821493d7399be65
Blake Mallory <blakecmallory@gmail.com>
2023-08-26 02:01:37 -0400

    check for bfloat16 and use appropriate data type

--- finetuning_repo/run_clm.py
+++ finetuning_repo/run_clm.py
@@ -399,7 +399,7 @@ def main():
         bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_compute_dtype=torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16,
             bnb_4bit_use_double_quant=True,
         )
     
