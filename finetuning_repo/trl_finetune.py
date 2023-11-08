added gradient checkpointing options, fixed flash attention bug, changed alpha default value

finetuning_repo/trl_finetune.py
@@ -13,6 +13,7 @@ import transformers
 from typing import Dict
 from llama_attn_replace import replace_llama_attn
 from typing import List, Optional
+from accelerate import Accelerator
 
 
 from utils import get_logger
@@ -75,6 +76,7 @@ def find_all_linear_names(args, model,add_lm_head=True):
     if add_lm_head and not "lm_head" in lora_module_names:
         logger.info("Adding lm_head to lora_module_names")
         lora_module_names.add("lm_head")
+
     return list(lora_module_names)
 
 def get_config(args):
@@ -85,6 +87,12 @@ def get_config(args):
     config = AutoConfig.from_pretrained(args.model_name, **config_kwargs)
 
     config.use_cache = False
+    if not args.gradient_checkpointing:
+        logger.info("Not using gradient checkpointing")
+        config.gradient_checkpointing = False
+    else:
+        logger.info("Using gradient checkpointing")
+        config.gradient_checkpointing = True
 
     orig_ctx_len = getattr(config, "max_position_embeddings", None)
     if orig_ctx_len and args.block_size > orig_ctx_len and args.rope_scale is None:
@@ -107,7 +115,7 @@ if __name__ == "__main__":
     parser.add_argument("--split_model", action="store_true",default=False)
     parser.add_argument("--block_size", type=int, default=128)
     parser.add_argument("--lora_rank", type=int, default=64)
-    parser.add_argument("--lora_alpha", type=int, default=16)
+    parser.add_argument("--lora_alpha", type=int, default=None)
     parser.add_argument("--lora_dropout", type=float, default=0.1)
 
     parser.add_argument("-lr","--learning_rate", type=float, default=1e-4)
@@ -147,6 +155,10 @@ if __name__ == "__main__":
 
     parser.add_argument("--completion_only", default=False,action="store_true", help="Only use completion loss")
     args = parser.parse_args()
+
+    if args.lora_alpha is None:
+        args.lora_alpha = args.lora_rank * 2
+        logger.info("Lora alpha set to None... Setting lora_alpha to %d", args.lora_alpha)
 
     # replace_llama_attn(use_full=False)
 
@@ -221,6 +233,9 @@ if __name__ == "__main__":
             bnb_4bit_compute_dtype=torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16,
             bnb_4bit_use_double_quant=True,
         )
+        device_index = Accelerator().process_index
+        device_map = {"": device_index}
+        kwargs["device_map"] = device_map
         optimizer = "adamw_bnb_8bit"
         args.use_int8 = False
     elif args.use_int8:
@@ -235,7 +250,7 @@ if __name__ == "__main__":
         optimizer = "adamw_torch"
 
     torch_dtype = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
-    model = AutoModelForCausalLM.from_pretrained(args.model_name, token=access_token,quantization_config=bnb_config,trust_remote_code=args.trust_remote_code,torch_dtype=torch_dtype,config=config,use_flash_attention_2=True, **kwargs)
+    model = AutoModelForCausalLM.from_pretrained(args.model_name, token=access_token,quantization_config=bnb_config,trust_remote_code=args.trust_remote_code,torch_dtype=torch_dtype,config=config,use_flash_attention_2=use_flash_attention, **kwargs)
     added_tokens = smart_tokenizer_and_embedding_resize(
         special_tokens_dict=special_tokens_dict,
         tokenizer=tokenizer,
@@ -272,7 +287,7 @@ if __name__ == "__main__":
         logger.info("Using LORA...")
         if args.use_int4 or args.use_int8:
             logger.info("Preparing model for kbit training...")
-            model = prepare_model_for_kbit_training(model)
+            model = prepare_model_for_kbit_training(model,use_gradient_checkpointing=True if args.gradient_checkpointing else False)
 
         logger.info("Getting PEFT model...")
         model = get_peft_model(model, peft_config)