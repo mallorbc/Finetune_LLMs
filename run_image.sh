3c3a00ab1d28bbe844abd4b5b7fa33fc9a8ea1a1
Blake Mallory <blakecmallory@gmail.com>
2023-11-10 03:50:47 +0000

    added support for zsh passthrough to make commands easier

--- run_image.sh
+++ run_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dir_to_mount=$(pwd)
-docker run -it --ipc=host --gpus all -v $HOME/.cache:/root/.cache -v $dir_to_mount:/workspace gpt
+docker run -it --ipc=host --gpus all -v $HOME/.cache:/root/.cache -v $HOME/.zshrc:/root/.zshrc -v $HOME/.zsh_history:/root/.zsh_history -v $dir_to_mount:/workspace gpt
